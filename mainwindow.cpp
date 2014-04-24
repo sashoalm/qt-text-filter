9c0d94cb2d90fa165038335fc45f69ce65ef7166
sashoalm <sashoalm@gmail.com>
2014-04-24 17:21:49 +0300

    First working version.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextStream>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QTimer *timer = new QTimer(this);
+    timer->setSingleShot(true);
+    timer->setInterval(1000);
+    connect(ui->lineEdit, SIGNAL(textEdited(QString)), timer, SLOT(start()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(on_lineEdit_editingFinished()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_lineEdit_editingFinished()
+{
+    const QString &query = ui->lineEdit->text();
+    if (query == lastQuery) {
+        return;
+    }
+
+    if (query.isEmpty()) {
+        ui->plainTextEdit->setReadOnly(false);
+        ui->plainTextEdit->setPlainText(unfilteredText);
+    } else {
+        ui->plainTextEdit->setReadOnly(true);
+        unfilteredText = ui->plainTextEdit->toPlainText();
+
+        QString filteredText;
+        QTextStream stream(&unfilteredText);
+        while (!stream.atEnd()) {
+            const QString &line = stream.readLine();
+            if (line.contains(query)) {
+                filteredText.append(line + "\n");
+            }
+        }
+
+        ui->plainTextEdit->setPlainText(filteredText);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_lineEdit_editingFinished();
+
 private:
     Ui::MainWindow *ui;
+    QString unfilteredText;
+    QString lastQuery;
 };
 
 #endif // MAINWINDOW_H
