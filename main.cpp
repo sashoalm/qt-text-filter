29a6908c815de537a7d227b1a52d4de01bac7316
Alexander Almaleh <sashoalm@gmail.com>
2014-09-23 12:24:40 +0300

    Add application icon.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setOrganizationName("CodeThesis");
     a.setApplicationName("TextFilter");
+    a.setWindowIcon(QIcon(":/appicon.png"));
     MainWindow w;
     w.show();
 
